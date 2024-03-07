18bc62bffabd58527b979be33328ecbdd092a544
David011e <davidoliver011e@gmail.com>
2024-03-07 10:06:23 +0000

    Customized admin

--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories' # To remove the s in the name in admin view
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
