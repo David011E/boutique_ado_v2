9319bbbd94a5c76d1c90850dc20a9a1996ac0c2f
David011e <davidoliver011e@gmail.com>
2024-03-08 10:01:41 +0000

    Added sorting JS and back to top link

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q  # To generate a search query
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -19,7 +20,7 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=('name'))
+                products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
