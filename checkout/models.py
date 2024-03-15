cafc067a9cbafa4cf4e7821ed09b076d8669d802
David011e <davidoliver011e@gmail.com>
2024-03-15 10:22:01 +0000

    Added profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -3,13 +3,17 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-from products.models import Product
+
 from django_countries.fields import CountryField
 
+from products.models import Product
+from profiles.models import UserProfile
 
-# Create your models here.
+
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -19,19 +23,19 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True) # Auto set the date for when a new date is created
+    date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
-    def generate_order_number(self):
+    def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
-    
+
     def update_total(self):
         """
         Update grand total each time a line item is added,
@@ -44,19 +48,18 @@ class Order(models.Model):
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
-    
+
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
-        if it hasn't been set already
+        if it hasn't been set already.
         """
         if not self.order_number:
-            self.order_number = self.generate_order_number()
+            self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
-    def __str__(self): # return the order number for the Order model
+    def __str__(self):
         return self.order_number
-
 
 
 class OrderLineItem(models.Model):
@@ -68,11 +71,11 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total 
+        Override the original save method to set the lineitem total
         and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-    def __str__(self): 
+    def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
