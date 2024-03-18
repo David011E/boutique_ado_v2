bfb50356e88ce6ac12dd0612edf636d2cd284cde
David011e <davidoliver011e@gmail.com>
2024-03-18 14:55:36 +0000

    updated email port

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-
 ALLOWED_HOSTS = ['*','.herokuapp.com']
 
 
@@ -215,7 +214,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 25
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
