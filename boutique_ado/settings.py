5fb55f7bdf4cb83dc55e97d8b4a214995da0f838
David011e <davidoliver011e@gmail.com>
2024-03-18 14:45:32 +0000

    Updated sending real emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','.herokuapp.com']
 
@@ -214,11 +215,11 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
+    EMAIL_PORT = 25
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'EMAIL_HOST_USER'
-    EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASS'
-    DEFAULT_FROM_EMAIL = 'EMAIL_HOST_USER'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # Default primary key field type
