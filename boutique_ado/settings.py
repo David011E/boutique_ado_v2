6e5a1857e6239933d3e0dfcfdd46f5289b80b22e
David011e <davidoliver011e@gmail.com>
2024-03-18 11:09:27 +0000

    Updated database

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -128,16 +128,8 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
  #     }
  # }
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
+DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
-else:
-    DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
