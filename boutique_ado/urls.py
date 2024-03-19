df519b872929bb9acd9a034b545ca0230d0d4fb5
David011e <davidoliver011e@gmail.com>
2024-03-19 09:39:11 +0000

    Added 404 page

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from boutique_ado.views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('handler404/', handler404),  # Use the handler404 view directly
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
